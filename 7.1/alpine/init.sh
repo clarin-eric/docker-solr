1cc20a23bc2660be73977f1db69bc59d1dbd0395
Twan Goosen <twan@clarin.eu>
2017-11-10 11:45:03 +0100

    Init script: Solr data

--- 7.1/alpine/init.sh
+++ 7.1/alpine/init.sh
@@ -4,7 +4,15 @@ DEFAULT_SOLR_DATA_HOME="/solr_data_home"
 DEFAULT_SOLR_HOME="/solr_home"
 
 function init_app() {
-	echo "Solr initialisation"
+	echo "Starting Solr initialisation"
+	
+	init_solr_home
+	init_solr_data
+	
+	echo "Solr initialisation completed"
+}
+
+function init_solr_home() {
 	if [ -z "$SOLR_HOME" ]; then
 		export SOLR_HOME=$DEFAULT_SOLR_DATA_HOME
 	fi
@@ -12,23 +20,33 @@ function init_app() {
 	if [ -e "$SOLR_HOME/solr.xml" ]; then
 		echo "Found existing Solr home at ${SOLR_HOME}, not touching it"
 	else
-		init_solr_home
-	fi
+		if [ ! -e "${SOLR_HOME}/solr.xml" ]; then
+			#Solr home does not yet exist, try to import
+			echo "Solr home directory $SOLR_HOME does not exist or is empty"
+			import_solr_home
+		fi
 	
-	#TODO: Solr data
+		if [ ! -e "${SOLR_HOME}/solr.xml" ]; then
+			echo "No solr home content was imported"
+			init_solr_home_from_template
+		fi
+	fi
 }
 
-function init_solr_home() {
-	if [ ! -e "${SOLR_HOME}/solr.xml" ]; then
-		#Solr home does not yet exist, try to import
-		echo "Solr home directory $SOLR_HOME does not exist or is empty"
-		import_solr_home
+function init_solr_data() {
+	if [ -z "$SOLR_DATA_HOME" ]; then
+		export SOLR_DATA_HOME=$DEFAULT_SOLR_DATA_HOME
 	fi
 	
-	if [ ! -e "${SOLR_HOME}/solr.xml" ]; then
-		echo "No solr home content was imported"
-		init_solr_home_from_template
+	#TODO: Check for content in SOLR_DATA_HOME....
+	if [ -e "$SOLR_DATA_HOME" ]; then
+		echo "Found existing Solr data home at ${SOLR_DATA_HOME}, not touching it"
+	else
+		echo "Solr home directory $SOLR_HOME does not exist or is empty"
+		import_solr_data
 	fi
+	
+	chown -R $SOLR_USER:$SOLR_GROUP "$SOLR_DATA_HOME"
 }
 
 function import_solr_home() {
@@ -38,6 +56,16 @@ function import_solr_home() {
 		cp -r "/docker-entrypoint-initsolr.d/solr_home"/* "$SOLR_HOME"
 		chown -R $SOLR_USER:$SOLR_GROUP "$SOLR_HOME"
 		echo "Initialised Solr home in ${SOLR_HOME}"
+	fi
+}
+
+function import_solr_data() {
+	if [ -d "/docker-entrypoint-initsolr.d/solr_data" ]; then
+		echo "Found Solr data to import"
+		purge_solr_data
+		cp -r "/docker-entrypoint-initsolr.d/solr_data"/* "$SOLR_DATA_HOME"
+		chown -R $SOLR_USER:$SOLR_GROUP "$SOLR_DATA_HOME"
+		echo "Initialised Solr data in ${SOLR_DATA_HOME}"
 	fi
 }
 
@@ -53,10 +81,19 @@ function init_solr_home_from_template() {
 
 function purge_solr_home() {
 	if [ -d "$SOLR_HOME" ]; then
-		echo "Removing original Solr home content at ${SOLR_HOME}"
-		rm -rf "${SOLR_HOME}"/*
+		echo "Removing any original Solr home content at ${SOLR_HOME}"
+		rm -rvf "${SOLR_HOME}"/*
 	else
 		mkdir -p "$SOLR_HOME"
+	fi
+}
+
+function purge_solr_data() {
+	if [ -d "$SOLR_DATA_HOME" ]; then
+		echo "Removing any original Solr data content at ${SOLR_DATA_HOME}"
+		rm -rvf "${SOLR_DATA_HOME}"/*
+	else
+		mkdir -p "$SOLR_DATA_HOME"
 	fi
 }
 
